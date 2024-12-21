91f117d538f6ab056e1c4053734eaf715fabcd67
changDDAO <younch8342@naver.com>
2024-12-21 15:52:48 +0900

    feat: Row component Props 받아서 처리하기

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "../api/axios";
 
-function Row(props) {
+function Row({title, id, isLargeRow,fetchUrl}) {
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        fetchMovieData();
+    }, []);
+
+    const fetchMovieData = async()=> {
+        const request = await axios.get(fetchUrl);
+        console.log('request',request);
+        setMovies(request.data.results);
+    }
     return (
-        <div>
+        <section>
+            <h2>${title}</h2>
+            <div className="slider">
+                <div className="slider__arrow-left">
+                    <span className="arrow">
+                        {"<"}
+                    </span>
+                </div>
+                <div id={id} className="row__poster">
+                    {movies.map(movie=>(
+                        <img key={movie.id}
+                             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        />
+                        )
+                        )
 
-        </div>
-    );
+                    }
+                </div>
+            </div>
+        </section>
+            );
 }
 
 export default Row;
