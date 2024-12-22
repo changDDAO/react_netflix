55fbbd70ed862437f4ad18043e453873cd0cfbc5
changDDAO <younch8342@naver.com>
2024-12-22 13:36:49 +0900

    feat: Row Component css 적용 및 슬라이드 효과

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from "../api/axios";
+import "./Row.css";
 
 function Row({title, id, isLargeRow,fetchUrl}) {
     const [movies, setMovies] = useState([])
@@ -13,7 +14,7 @@ function Row({title, id, isLargeRow,fetchUrl}) {
         setMovies(request.data.results);
     }
     return (
-        <section>
+        <section className="row">
             <h2>${title}</h2>
             <div className="slider">
                 <div className="slider__arrow-left">
@@ -21,15 +22,19 @@ function Row({title, id, isLargeRow,fetchUrl}) {
                         {"<"}
                     </span>
                 </div>
-                <div id={id} className="row__poster">
+                <div id={id} className="row__posters">
                     {movies.map(movie=>(
                         <img key={movie.id}
                              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                             src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                             alt={movie.name}
                         />
                         )
                         )
-
                     }
+                </div>
+                <div className="slider__arrow-right">
+                    <span className="arrow">{">"}</span>
                 </div>
             </div>
         </section>
