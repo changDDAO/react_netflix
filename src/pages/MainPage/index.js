480c6af4972a4ae845c16c312e46cbdab4aabfba
changDDAO <younch8342@naver.com>
2024-12-23 23:44:19 +0900

    refactor: react-router-dom을 이용한 routing 및 Banner 및 Nav Bar css reafctoring

---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Banner from "../../components/Banner";
+import Row from "../../components/Row";
+import requests from "../../api/request";
+
+function MainPage(props) {
+    return (
+        <div>
+            <Banner/>
+            <Row title="NETFLIX ORIGINAL"
+                 id="NO"
+                 fetchUrl={requests.fetchNetflixOriginals}
+                 isLargeRow
+            />
+            <Row title="TrendingNow" id="TN" fetchUrl={requests.fetchTrending}/>
+            <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}/>
+            <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}/>
+            <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}/>
+
+        </div>
+    );
+}
+
+export default MainPage;
