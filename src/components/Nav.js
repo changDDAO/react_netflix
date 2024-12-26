6e7323d0da42d234d57fc5799130b865cf85884a
changDDAO <younch8342@naver.com>
2024-12-26 23:53:32 +0900

    feat: useLocation() function을 활용한 url에 동적으로 검색값 넘겨주기

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import "./Nav.css";
+import {useNavigate} from "react-router-dom";
 function Nav(props) {
     const[show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const navigate = useNavigate();
 
     useEffect(()=>{
         window.addEventListener("scroll",()=>{
@@ -17,6 +20,12 @@ function Nav(props) {
             };
         })
     },[]);
+
+    function handleChange(e) {
+        setSearchValue(e.target.value);
+        navigate(`search?q=${e.target.value}`);
+    }
+
     return (
         <nav className={`nav ${show && "nav__black"}`}>
             <img
@@ -24,6 +33,12 @@ function Nav(props) {
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png"
                 className="nav__logo"
                 onClick={() => window.location.reload()}
+            />
+            <input value={searchValue}
+                   onChange={handleChange}
+                   className="nav__input"
+                   type="text"
+                   placeholder="영화를 검색해주세요."
             />
             <img
                 alt="User logged"
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useLocation} from "react-router-dom";
+import axios from "../../api/axios";
+
 
 function SearchPage(props) {
+    const [searchResults, setSearchResults] = useState([]);
+    const useQuery = () => {
+
+        return new URLSearchParams(useLocation().search);
+    };
+    let query = useQuery();
+    const searchTerm = query.get("q");
+    console.log('searchTerm', searchTerm);
+    useEffect(() => {
+        if (searchTerm) {
+            fetchSearchMovie();
+        }
+    }, [searchTerm]);
+
+    const fetchSearchMovie = async (searchTerm) =>{
+        try{
+            const request = await axios.get(
+                `/search/multi?include_adult=false&query=${searchTerm}`
+            );
+            console.log(request);
+            setSearchResults(request.data.results);
+
+        }catch(error){
+            console.log("error", error);
+        }
+    }
+
+
     return (
         <div>
 
