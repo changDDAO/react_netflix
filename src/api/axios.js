977e2672401795e8270217941e46d8487683042c
changDDAO <younch8342@naver.com>
2024-12-29 12:25:20 +0900

    refactor: api key env File에 담아서 노출하지 않기

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL : "https://api.themoviedb.org/3",
     params : {
-        api_key : "bf31d3d1c8630a6fc75fbc13358ad5bd",
+        api_key : process.env.REACT_APP_MOVIE_DB_API_KEY,
         language : "ko-KR",
     },
 });
